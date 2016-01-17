8bf19be3e651343b4fe2afbb7738ee8ddda9c29f
chappylol <mchap88@gmail.com>
2016-01-17 17:58:31 -0600

    changing more range bs. wish there was an easier way to do this!

--- png_minecraft.py
+++ png_minecraft.py
@@ -108,7 +108,7 @@ def parse_chunk(chunk):
         mod_path = re.sub('\.png.*$','', mod_path)
 
         mins = "%s,%s,%s" % (x,y,0)
-        maxes = "%s,%s,%s" % (x,y,1)
+        maxes = "%s,%s,%s" % (x+1,y+1,1)
         mod_string = "%s:%s" % (mod_name, mod_path)
         pixel_str = "    { %s,%s,texture=\"%s\"}," % (mins, maxes, mod_string)
 
